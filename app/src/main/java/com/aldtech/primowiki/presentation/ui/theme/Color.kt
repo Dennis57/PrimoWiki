32fb2517a16bf59be6856c9b06b761ce9d2317ec
Dennis Chandrian <Dennischandrian@gmail.com>
2023-09-30 18:53:23 +0700

    Character Screen progress 1

--- app/src/main/java/com/aldtech/primowiki/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/aldtech/primowiki/presentation/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryWhite = Color(0xFFF7f3DF)
+val SecondaryWhite = Color(0xFFEDEBD5)
+
+val PrimaryText = Color(0xFFABA589)
